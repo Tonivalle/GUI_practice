efec0cb61f9dff4c98231bfc20465dab33155de1
Toni <tonivalle15@gmail.com>
2018-10-04 12:29:52 +0200

    Image and bg

--- Tkinter1.py
+++ Tkinter1.py
@@ -1,6 +1,17 @@
+
+#importamos tkinter
 from tkinter import *
+
+#abrir la ventana y titulo
 
 window = Tk()
 window.title("GUI")
+#configurando el color de fondo
+window.configure(background='black')
+#imagen
+
+photo = PhotoImage(file='Allmight.png')
+Label (window, image = photo, bg='black') .grid(row=0, column=0, sticky=W)
+#loop principal
 
 window.mainloop()
